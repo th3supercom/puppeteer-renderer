c2e21f48c02c8f54ebb1a09830c70718a14bd74c
th3supercom <th3super.pro@gmail.com>
2018-01-23 10:03:16 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 const express = require('express')
 const createRenderer = require('./renderer')
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
 const app = express()
 
